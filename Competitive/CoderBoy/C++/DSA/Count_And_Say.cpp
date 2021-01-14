cfbe33ea4c20059cc4c7ac156b01444ae2f830f0
PRITHWIRAJ SAMANTA <prskid1000@gmail.com>
2021-01-14 14:55:00 +0530

    Code Added

--- Competitive/CoderBoy/C++/DSA/Count_And_Say.cpp
+++ Competitive/CoderBoy/C++/DSA/Count_And_Say.cpp
@@ -35,7 +35,7 @@ int main()
       int n = 0;
       cin >> n;
 
-        string s = "1", ans = "";
+      string s = "1", ans = "";
         for(int i = 0; i < n ; i++)
         {
             for(int j = 0; j < s.length(); j++)
